0a4ecdd7661bb712d39a676a752f60cbc1a6b196
jericmac <jeric.goya@gmail.com>
2019-02-02 14:17:49 +1100

    Western Australia Gas Bulletin Board API

---
+++ energy-trading-api-wrappers/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="energy_trading_api_wrappers",
+    version="0.0.1",
+    author="jericmac",
+    author_email="",
+    description="API Wrappers for the Energy Markets",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/jericmac/energy-trading-api-wrappers",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)

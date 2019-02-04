e81e559af9402f595b755d90b29b81f191670051
jericmac <jeric.goya@gmail.com>
2019-02-04 14:46:37 +1100

    PyTest refactoring

--- testing/test_wagbb.py
+++ testing/test_wagbb.py
@@ -2,13 +2,22 @@ from energy_trading_api import wagbb as w
 from energy_trading_api import jepx as j
 
 
+def test_actualFlow():
+    # load capacity outlook
+    assert not w.actualFlow().empty
+
+def test_actualFlowDate(gasDay="2018-01-01"):
+    # load capacity outlook
+    assert not w.actualFlow(gasDay).empty
+
+
 def test_ForecastFlow():
     # load capacity outlook
     assert not w.forecastFlow().empty
 
-def test_ForecastFlowDate():
+def test_ForecastFlowDate(gasDay="2018-01-01"):
     # load capacity outlook
-    assert not w.forecastFlow(gasDay="2018-01-01").empty
+    assert not w.forecastFlow(gasDay).empty
 
 
 

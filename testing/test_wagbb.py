e876f363033831a1dc39b1029fae0f91d403ba18
jericmac <jeric.goya@gmail.com>
2019-02-04 14:56:51 +1100

    PyTest refactoring

--- testing/test_wagbb.py
+++ testing/test_wagbb.py
@@ -10,15 +10,6 @@ def test_actualFlowDate(gasDay="2018-01-01"):
     # load capacity outlook
     assert not w.actualFlow(gasDay).empty
 
-
-def test_ForecastFlow():
-    # load capacity outlook
-    assert not w.forecastFlow().empty
-
-def test_ForecastFlowDate(gasDay="2018-01-01"):
-    # load capacity outlook
-    assert not w.forecastFlow(gasDay).empty
-
 def test_endUserConsumption():
     # load capacity outlook
     assert not w.endUserConsumption().empty
@@ -50,3 +41,36 @@ def test_gasSpecification():
 def test_gasSpecificationDate(gasDay="2018-01-01"):
     # load capacity outlook
     assert not w.largeUserConsumptionByCategory(gasDay).empty
+
+def test_largeUserConsumption():
+    # load capacity outlook
+    assert not w.largeUserConsumption().empty
+
+def test_largeUserConsumptionDate(gasDay="2018-01-01"):
+    # load capacity outlook
+    assert not w.largeUserConsumption(gasDay).empty
+
+def test_capacityOutlook():
+    # load capacity outlook
+    assert not w.capacityOutlook().empty
+
+def test_capacityOutlookDate(gasDay="2018-01-01"):
+    # load capacity outlook
+    assert not w.capacityOutlook(gasDay).empty
+
+def test_linepackCapacityAdequacy():
+    # load capacity outlook
+    assert not w.linepackCapacityAdequacy().empty
+
+def test_linepackCapacityAdequacyDate(gasDay="2018-01-01"):
+    # load capacity outlook
+    assert not w.linepackCapacityAdequacy(gasDay).empty
+
+
+def test_ForecastFlow():
+    # load capacity outlook
+    assert not w.forecastFlow().empty
+
+def test_ForecastFlowDate(gasDay="2018-01-01"):
+    # load capacity outlook
+    assert not w.forecastFlow(gasDay).empty
